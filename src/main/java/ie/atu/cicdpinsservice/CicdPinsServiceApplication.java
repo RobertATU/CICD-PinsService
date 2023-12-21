143b8998d9044dee8ceaa760f3c6df37c6926b3b
Robert <g00395612@atu.ie>
2023-12-21 16:48:36 +0000

    https://github.com/RobertATU/CICD-PinsService.git

---
+++ src/main/java/ie/atu/cicdpinsservice/CicdPinsServiceApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.cicdpinsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdPinsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdPinsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ie/atu/cicdpinsservice/CicdPinsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.cicdpinsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdPinsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
